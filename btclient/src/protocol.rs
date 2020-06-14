49c4e235cf010a6a23367e3eff921d6e06ea2d4d
Patrick Breen <breen.patrick.1010@gmail.com>
2020-06-13 20:40:00 -0400

    add more protocols

--- btclient/src/protocol.rs
+++ btclient/src/protocol.rs
@@ -21,6 +21,10 @@
 // Cancel
 
 
+// TODO!!!
+// implement PeerConnection, PeerStreamIterator, and Test
+
+
 const REQUEST_SIZE: usize = 16384;
 
 // TODO
@@ -179,6 +183,238 @@ impl PeerMessage for BitField {
     }
     fn to_string() -> String {
         "BitField".to_string()
+    }
+}
+
+struct Interested {
+}
+
+impl PeerMessage for Interested {
+    // The interested message is fix length and has no payload other than the
+    // message identifiers. It is used to notify each other about interest in
+    // downloading pieces.
+
+    fn encode(&mut self) -> Vec<u8> {
+        let s = structure!(">Ib");
+        s.pack(1, BitField::INTERESTED as i8).unwrap()
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        panic!("Not implemented");
+    }
+    fn to_string() -> String {
+        "Interested".to_string()
+    }
+}
+
+
+struct NotInterested {
+}
+
+impl PeerMessage for NotInterested {
+    // The not interested message is fix length and has no payload other than the
+    // message identifier. It is used to notify each other that there is no
+    // interest to download pieces.
+
+    fn encode(&mut self) -> Vec<u8> {
+        panic!("Not implemented");
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        panic!("Not implemented");
+    }
+    fn to_string() -> String {
+        "NotInterested".to_string()
+    }
+}
+
+
+struct Choke {
+}
+
+impl PeerMessage for Choke {
+    // The choke message is used to tell the other peer to stop send request
+    // messages until unchoked.
+
+    fn encode(&mut self) -> Vec<u8> {
+        panic!("Not implemented");
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        panic!("Not implemented");
+    }
+    fn to_string() -> String {
+        "Choke".to_string()
+    }
+}
+
+
+
+struct UnChoke {
+}
+
+impl PeerMessage for UnChoke {
+    // Unchoking a peer enables that peer to start requesting pieces from the
+    // remote peer.
+
+    fn encode(&mut self) -> Vec<u8> {
+        panic!("Not implemented");
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        panic!("Not implemented");
+    }
+    fn to_string() -> String {
+        "UnChoke".to_string()
+    }
+}
+
+
+
+struct Have {
+    index: u32,
+}
+
+impl Have {
+    fn new(index: u32) -> Have {
+        Have { index: index }
+    }
+}
+
+impl PeerMessage for Have {
+    // Represents a piece successfully downloaded by the remote peer. The piece
+    // is a zero based index of the torrents pieces
+
+    fn encode(&mut self) -> Vec<u8> {
+        let s = structure!(">IbI");
+        s.pack(5, BitField::HAVE as i8, self.index).unwrap()
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        let s = structure!(">IbI");
+        let  (message_length, message_type, index) = s.unpack(data).unwrap();
+
+        Have::new(index)
+    }
+    fn to_string() -> String {
+        "Have".to_string()
+    }
+}
+
+
+
+struct Request {
+    index: u32,
+    begin: u32,
+    length: u32,
+}
+
+impl Request {
+    fn new(index: u32, begin: u32, length: u32) -> Request {
+        Request {
+            index: index,
+            begin: begin,
+            length: length,
+        }
+    }
+}
+
+impl PeerMessage for Request {
+    // The message used to request a block of a piece (i.e. a partial piece).
+    // The request size for each block is 2^14 bytes, except the final block
+    // that might be smaller (since not all pieces might be evenly divided by the
+    // request size).
+
+    fn encode(&mut self) -> Vec<u8> {
+        let s = structure!(">IbIII");
+        s.pack(13, BitField::REQUEST as i8, self.index, self.begin, self.length).unwrap()
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        let s = structure!(">IbIII");
+        let  (message_length, message_type, index, begin, length) = s.unpack(data).unwrap();
+
+        Request::new(index, begin, length)
+    }
+    fn to_string() -> String {
+        "Request".to_string()
+    }
+}
+
+
+struct Piece {
+    index: u32,
+    begin: u32,
+    block: Vec<u8>,
+}
+
+impl Piece {
+    const LENGTH: usize = 9;
+
+
+    fn new(index: u32, begin: u32, block: Vec<u8>) -> Piece {
+        Piece {
+            index: index,
+            begin: begin,
+            block: block
+        }
+    }
+}
+
+impl PeerMessage for Piece {
+    // A block is a part of a piece mentioned in the meta-info. The official
+    // specification refer to them as pieces as well - which is quite confusing
+    // the unofficial specification refers to them as blocks however.
+
+    // So this class is named `Piece` to match the message in the specification
+    // but really, it represents a `Block` (which is non-existent in the spec).
+
+    fn encode(&mut self) -> Vec<u8> {
+        let message_length = Piece::LENGTH + self.block.len();
+        let s = structure!(">IbII");
+        let mut buf = s.pack(message_length as u32, BitField::PIECE as i8, self.index, self.begin).unwrap();
+        buf.append(&mut self.block.clone()); // clone so that block isn't emptied
+        buf
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        let s = structure!(">IbII");
+        let  (message_length, message_type, index, begin) = s.unpack(data[..13].to_vec()).unwrap();
+        let block = data[13..].to_vec();
+
+        Piece::new(index, begin, block)
+    }
+    fn to_string() -> String {
+        "Piece".to_string()
+    }
+}
+
+
+struct Cancel {
+    index: u32,
+    begin: u32,
+    length: u32,
+}
+
+impl Cancel {
+    fn new(index: u32, begin: u32, length: u32) -> Cancel {
+        Cancel {
+            index: index,
+            begin: begin,
+            length: length,
+        }
+    }
+}
+
+impl PeerMessage for Cancel {
+    // The cancel message is used to cancel a previously requested block (in fact
+    // the message is identical (besides from the id) to the Request message).
+
+    fn encode(&mut self) -> Vec<u8> {
+        let s = structure!(">IbIII");
+        s.pack(13, BitField::CANCEL as i8, self.index, self.begin, self.length).unwrap()
+    }
+    fn decode(data: Vec<u8>) -> Self {
+        let s = structure!(">IbIII");
+        let  (message_length, message_type, index, begin, length) = s.unpack(data).unwrap();
+
+        Cancel::new(index, begin, length)
+    }
+    fn to_string() -> String {
+        "Cancel".to_string()
     }
 }
 
